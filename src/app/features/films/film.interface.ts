f036216f094934f2014216bc05ae31c8e5ad71e0
prokhorovd <github@prokhorov.dev>
2026-05-02 20:56:40 +0700

    feat: films service

---
+++ src/app/features/films/film.interface.ts
@@ -0,0 +1,11 @@
+export interface Film {
+  id: number; // 1,
+  title: string; // "Interstellar",
+  year: number; // 2014,
+  genre: string; // "Sci-Fi",
+  rating: number; // 8.6,
+  duration: number; // 169,
+  description: string; // "When drought, dust storms, and the extinction of crops lead humanity to a food crisis, a team of explorers and scientists travels through a wormhole in search of a new home for mankind.",
+  posterUrl: string; // "https://placehold.co/300x450?text=Interstellar",
+  isFavorite: boolean; // false
+}
