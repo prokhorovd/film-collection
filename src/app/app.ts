d522d40340762458e0d264906e6e7b786873e7b6
prokhorovd <github@prokhorov.dev>
2026-05-02 19:55:56 +0700

    feat: init project structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Breadcrumbs } from './layout/breadcrumbs/breadcrumbs';
+import { Footer } from './layout/footer/footer';
+import { Header } from './layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Breadcrumbs, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('film-collection');
---
+++ src/app/layout/breadcrumbs/breadcrumbs.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcrumbs',
+  imports: [],
+  templateUrl: './breadcrumbs.html',
+  styleUrl: './breadcrumbs.css',
+})
+export class Breadcrumbs {}
