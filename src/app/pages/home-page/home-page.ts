b42f08b8dcfbc30623680cd6d981a7e9f7665923
prokhorovd <github@prokhorov.dev>
2026-05-02 20:58:31 +0700

    feat: show film list on the home page

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FilmCard } from '../../features/films/components/film-card/film-card';
+import { FilmService } from '../../features/films/film.service';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [FilmCard],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
-export class HomePage {}
+export class HomePage {
+  private readonly filmService = inject(FilmService);
+
+  readonly films = this.filmService.allFilms;
+
+  toggleFavorite(id: number): void {
+    this.filmService.toggleFavorite(id);
+  }
+}
