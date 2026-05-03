03798049c763e4a6a239337088ad068184207aca
prokhorovd <github@prokhorov.dev>
2026-05-03 16:57:30 +0700

    feat: movies search field

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { FilmCard } from '../../features/films/components/film-card/film-card';
 import { FilmService } from '../../features/films/film.service';
 
@@ -15,5 +15,19 @@ export class HomePage {
 
   toggleFavorite(id: number): void {
     this.filmService.toggleFavorite(id);
+  }
+
+  searchQuery = signal('');
+
+  readonly filteredFilms = computed(() => {
+    const query = this.searchQuery().trim().toLowerCase();
+
+    return this.films().filter((film) => film.title.toLowerCase().includes(query));
+  });
+
+  onSearchChange(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    this.searchQuery.set(input.value);
   }
 }
