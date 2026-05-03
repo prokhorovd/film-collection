9d7accd9fbfedbdf6da6922b77425dee710af611
prokhorovd <github@prokhorov.dev>
2026-05-03 19:43:47 +0700

    feat: breadcrumbs

--- src/app/layout/breadcrumbs/breadcrumbs.ts
+++ src/app/layout/breadcrumbs/breadcrumbs.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
+import { FilmService } from '../../features/films/film.service';
+
+type Breadcrumb = {
+  label: string;
+  url?: string;
+};
 
 @Component({
   selector: 'app-breadcrumbs',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './breadcrumbs.html',
   styleUrl: './breadcrumbs.css',
 })
-export class Breadcrumbs {}
+export class Breadcrumbs {
+  private readonly router = inject(Router);
+  private readonly filmService = inject(FilmService);
+
+  readonly breadcrumbs = computed<Breadcrumb[]>(() => {
+    this.router.lastSuccessfulNavigation();
+
+    const [, segment, id] = this.router.url.split('/');
+
+    if (!segment) {
+      return [{ label: 'Home' }];
+    }
+
+    if (segment === 'films' && id) {
+      const film = this.filmService.getFilmById(Number(id));
+
+      return [{ label: 'Home', url: '/' }, { label: film?.title ?? 'Film not found' }];
+    }
+
+    return [{ label: 'Home', url: '/' }, { label: '404' }];
+  });
+}
